57fa5871444422422add644702f0834949ff7b82
mmcdaniel44 <mmcdaniel44@gatech.edu>
2022-02-22 17:33:47 -0500

    adding api response to components

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   const search = evt => {
     if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric$&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
       .then(response => response.json())
       .then(result => {
         setWeather(result);
@@ -26,7 +26,7 @@ function App() {
 
   const dateBuilder = (d) => {
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
-    'September', 'October', 'N;ovemmber', 'December']
+    'September', 'October', 'Novemmber', 'December']
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     let day = days[d.getDay()];
@@ -63,10 +63,10 @@ function App() {
         </div>
         <div className="weather-box">
           <div className="temp">
-            15°c
+           {Math.round(weather.main.temp)}°F
           </div>
           <div className="weather">
-            Sunny
+            {weather.weather[0].main}
           </div>
         </div>
         </div>
